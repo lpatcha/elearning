f28fe9cfdadcd426193bb3ce4d9501e4f650d7d9
Ganesh Reddy Puli <puliganeshreddy11@gmail.com>
2023-10-18 14:34:47 -0500

    "Added sending mails to users" functionality

--- Frontend/src/app/admin/admin.component.ts
+++ Frontend/src/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LogoutService } from '../logout.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'app-admin',
@@ -7,7 +9,7 @@ import { Router } from '@angular/router';
     styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
-    constructor(private router: Router) {}
+    constructor(private router: Router,private logoutService: LogoutService,private http: HttpClient) {}
 
     showCreateUserForm() {
         this.router.navigate(['/create']);
@@ -16,4 +18,23 @@ export class AdminComponent {
     showCreateMultipleUsersForm() {
         this.router.navigate(['/upload-excel']);
     }
+    sentmail(){
+        console.log("hi");
+        this.http.post('http://localhost:8080/api/admin/sentmail',null).subscribe(
+            (response: any) => {
+                console.log("hiiiiii");
+                if (response.message === 'Mails sent successfully.') {
+                    console.log('Registration email sent to successful');
+                  } else {
+                    console.error('Admin creation failed.');
+                  }
+            },
+            (error: any) => {
+                console.error('An error occurred while creating the admin:', error);
+              })
+    }
+
+    logout() {
+        this.logoutService.logout();
+      }
 }
